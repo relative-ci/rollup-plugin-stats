4dd40294c3edf8953b405e049610e7544fce829e
Viorel Cojocaru <vio@beanon.com>
2025-06-29 00:22:49 +0200

    test: Add test package for vite

---
+++ test/package/vite/vite-package.test.js
@@ -0,0 +1,45 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { describe, test, expect, beforeEach } from 'vitest';
+import { build as vite } from 'vite';
+import { vol } from 'memfs';
+
+import config, { relativeFileNameConfig, absoluteFileNameConfig } from './vite.config';
+
+describe('vite package test', () => {
+  beforeEach(() => {
+    vol.reset();
+  });
+
+  test('should output stats JSON file', async () => {
+    await vite(config);
+
+    const actual = await fs.readFile(path.join(__dirname, config.output.dir, 'stats.json'), 'utf8');
+    const stats = JSON.parse(actual);
+    expect(stats['assets/index.js']).toMatchObject({
+      fileName: 'assets/index.js',
+    });
+  });
+
+  test('should output stats JSON file with custom relative filename', async () => {
+    await vite(relativeFileNameConfig);
+
+    const actual = await fs.readFile(path.join(__dirname, relativeFileNameConfig.output.dir, '../artifacts/stats-relative-path.json'), 'utf8');
+    const stats = JSON.parse(actual);
+
+    expect(stats['assets/index.js']).toMatchObject({
+      fileName: 'assets/index.js',
+    });
+  });
+
+  test('should output stats JSON file with custom absolute filename', async () => {
+    await vite(absoluteFileNameConfig);
+
+    const actual = await fs.readFile('/tmp/custom-stats.json', 'utf8');
+    const stats = JSON.parse(actual);
+
+    expect(stats['assets/index.js']).toMatchObject({
+      fileName: 'assets/index.js',
+    });
+  });
+});
