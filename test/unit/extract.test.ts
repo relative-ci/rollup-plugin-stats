8424c2846383721833e78e1d1a2a00ab60dd118a
Viorel Cojocaru <vio@beanon.com>
2025-08-04 18:03:31 +0200

    test: Deep freeze bundle stats during testing

--- test/unit/extract.test.ts
+++ test/unit/extract.test.ts
@@ -1,26 +1,23 @@
 import { describe, test, expect } from 'vitest';
+import deepFreeze from 'deep-freeze-strict';
 
 import extract from '../../src/extract';
 import * as rollupStats from './fixtures/rollup-stats';
 
 describe('extract', () => {
   test('should extract rollup stats', () => {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    expect(extract(rollupStats.stats as any)).toMatchSnapshot();
+    expect(extract(deepFreeze(rollupStats.stats))).toMatchSnapshot();
   });
 
   test('should extract rollup stats with sources', () => {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    expect(extract(rollupStats.stats as any, { source: true })).toMatchSnapshot();
+    expect(extract(deepFreeze(rollupStats.stats), { source: true })).toMatchSnapshot();
   });
 
   test('should extract rollup stats with excluded assets', () => {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    expect(extract(rollupStats.stats as any, { excludeAssets : /vendors/ })).toMatchSnapshot();
+    expect(extract(deepFreeze(rollupStats.stats), { excludeAssets : /vendors/ })).toMatchSnapshot();
   });
 
   test('should extract rollup stats with excluded modules', () => {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    expect(extract(rollupStats.stats as any, { excludeModules : /utils.js/ })).toMatchSnapshot();
+    expect(extract(deepFreeze(rollupStats.stats), { excludeModules : /utils.js/ })).toMatchSnapshot();
   });
 });
