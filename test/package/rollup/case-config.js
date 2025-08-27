d53992277c7dc66f83a26a4fb95bd32acbd7350c
Viorel Cojocaru <vio@beanon.com>
2025-08-27 23:13:35 +0200

    test: Rollup - add cases for commonjs, mjs and ts configs

---
+++ test/package/rollup/case-config.js
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+const { defineConfig } = require('rollup');
+const stats = require('rollup-plugin-stats');
+
+module.exports = defineConfig({
+  input: 'src/index.js',
+  output: {
+    dir: 'dist',
+  },
+  plugins: [stats({})],
+});
