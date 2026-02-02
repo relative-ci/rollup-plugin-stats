9ff528c78b8667283d38271b4e8fb00ae6070805
Viorel Cojocaru <vio@beanon.com>
2026-02-02 23:08:39 +0100

    fix: Export plugin types from vite & rollup

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import path from 'node:path';
 import process from 'node:process';
-import type { Plugin, OutputOptions } from 'rollup';
 
 import extractRollupStats, { type StatsOptions } from './extract';
 import { type RollupStatsWrite, rollupStatsWrite } from './write';
 import { formatFileSize } from './utils/format-file-size';
+import type { Plugin, OutputOptions } from './types';
 
 const PLUGIN_NAME = 'rollupStats';
 const DEFAULT_FILE_NAME = 'stats.json';
@@ -30,13 +30,17 @@ type RollupStatsOptionsOrOutputOptions =
   | RollupStatsOptions
   | ((outputOptions: OutputOptions) => RollupStatsOptions);
 
-
 function rollupStats(options: RollupStatsOptionsOrOutputOptions = {}): Plugin {
   return {
     name: PLUGIN_NAME,
     async generateBundle(context, bundle) {
-      const resolvedOptions = typeof options === 'function' ? options(context) : options;
-      const { fileName, stats: statsOptions, write = rollupStatsWrite } = resolvedOptions || {};
+      const resolvedOptions =
+        typeof options === 'function' ? options(context) : options;
+      const {
+        fileName,
+        stats: statsOptions,
+        write = rollupStatsWrite,
+      } = resolvedOptions || {};
 
       const resolvedFileName = fileName || DEFAULT_FILE_NAME;
       const filepath = path.isAbsolute(resolvedFileName)
@@ -49,14 +53,18 @@ function rollupStats(options: RollupStatsOptionsOrOutputOptions = {}): Plugin {
         const res = await write(filepath, stats);
         const outputSize = Buffer.byteLength(res.content, 'utf-8');
 
-        this.info(`Stats saved to ${res.filepath} (${formatFileSize(outputSize)})`);
-      } catch (error: any) { // eslint-disable-line
+        this.info(
+          `Stats saved to ${res.filepath} (${formatFileSize(outputSize)})`
+        );
+      } catch (error: unknown) {
+        const message =
+          error instanceof Error ? error.message : JSON.stringify(error);
+
         // Log error, but do not throw to allow the compilation to continue
-        this.warn(error);
+        this.warn(message);
       }
     },
   } satisfies Plugin;
 }
 
 export default rollupStats;
-
